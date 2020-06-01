957b711274d8a2e663a069f0041a6f8aa0d9c7b9
Helena Brito <helena_b_dias@hotmail.com>
2020-06-01 15:36:59 -0300

    Special move - Castling

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -113,6 +113,24 @@ public class ChessMatch {
 			capturedPieces.add(capturedPiece);
 		}
 		
+		//Movimento roque lado do rei e mover a torre manualmente
+		if(p instanceof King && target.getColumn() == source.getColumn() + 2) {
+			Position sourceT = new Position(source.getRow(), source.getColumn() + 3);//peguei a posição da torre
+			Position targetT = new Position(source.getRow(), source.getColumn() + 1);//uma posição antes do rei para mover a torre
+			ChessPiece rook = (ChessPiece)board.removePiece(sourceT);//retirar a torre de onde ela está
+			board.placePiece(rook, targetT);//colocar a torre na posição de destino dela
+			rook.increaseMoveCount();
+		}
+		
+		//Movimento roque lado da dama e mover torre manualmente
+		if (p instanceof King && target.getColumn() == source.getColumn() - 2) {
+			Position sourceT = new Position(source.getRow(), source.getColumn() - 4);// peguei a posição da torre
+			Position targetT = new Position(source.getRow(), source.getColumn() - 1);// uma posição antes do rei para mover a torre
+			ChessPiece rook = (ChessPiece) board.removePiece(sourceT);// retirar a torre de onde ela está
+			board.placePiece(rook, targetT);// colocar a torre na posição de destino dela
+			rook.increaseMoveCount();
+		}
+		
 		return capturedPiece;//retorna a peça capturada
 	}
 	
@@ -128,6 +146,24 @@ public class ChessMatch {
 			//Tenho que tirar essa peça da lista de peças capturadas e coloca-lá novamente na lista de peças do tabuleiro
 			capturedPieces.remove(capturedPiece);
 			piecesOnTheBoard.add(capturedPiece);
+		}
+		
+		//Movimento roque lado do rei e mover a torre manualmente
+		if (p instanceof King && target.getColumn() == source.getColumn() + 2) {
+			Position sourceT = new Position(source.getRow(), source.getColumn() + 3);// peguei a posição da torre
+			Position targetT = new Position(source.getRow(), source.getColumn() + 1);// uma posição antes do rei para mover a torre
+			ChessPiece rook = (ChessPiece) board.removePiece(targetT);// retirar a torre da posição de destino
+			board.placePiece(rook, sourceT);// colocar a torre na posição de origem dela
+			rook.decreaseMoveCount();
+		}
+
+		// Movimento roque lado da dama e mover torre manualmente
+		if (p instanceof King && target.getColumn() == source.getColumn() - 2) {
+			Position sourceT = new Position(source.getRow(), source.getColumn() - 4);// peguei a posição da torre
+			Position targetT = new Position(source.getRow(), source.getColumn() - 1);// uma posição antes do rei para mover a torre
+			ChessPiece rook = (ChessPiece) board.removePiece(targetT);// retirar a torre da posição de destino
+			board.placePiece(rook, sourceT);// colocar a torre na posição de origem
+			rook.decreaseMoveCount();
 		}
 	}
 	
@@ -235,7 +271,7 @@ public class ChessMatch {
 		placeNewPiece('b', 1, new Knight(board, Color.WHITE));
 		placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
 		placeNewPiece('d', 1, new Queen(board, Color.WHITE));
-		placeNewPiece('e', 1, new King(board, Color.WHITE));
+		placeNewPiece('e', 1, new King(board, Color.WHITE, this));
 		placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
 		placeNewPiece('g', 1, new Knight(board, Color.WHITE));
 		placeNewPiece('h', 1, new Rook(board, Color.WHITE));
@@ -252,7 +288,7 @@ public class ChessMatch {
 		placeNewPiece('b', 8, new Knight(board, Color.BLACK));
 		placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
 		placeNewPiece('d', 8, new Queen(board, Color.BLACK));
-		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('e', 8, new King(board, Color.BLACK, this));
 		placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
 		placeNewPiece('g', 8, new Knight(board, Color.BLACK));
 		placeNewPiece('h', 8, new Rook(board, Color.BLACK));
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -2,13 +2,17 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
 
-	public King(Board board, Color color) {
+	private ChessMatch chessMatch;
+	
+	public King(Board board, Color color, ChessMatch chessMatch) {
 		super(board, color);
+		this.chessMatch = chessMatch;
 	}
 
 	@Override
@@ -20,6 +24,12 @@ public class King extends ChessPiece {
 	private boolean canMove(Position position) {
 		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p == null || p.getColor() != getColor();
+	}
+	
+	//Método auxiliar para testar a condição de roque, se existe uma torre na posição indicada e se ela está apta para o roque
+	private boolean testRookCastling(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);//peguei a peça da posição indicada
+		return p != null && p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0;
 	}
 
 	@Override
@@ -75,6 +85,35 @@ public class King extends ChessPiece {
 		p.setValues(position.getRow() + 1, position.getColumn() + 1);
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Movimento especial Roque
+		if(getMoveCount() == 0 && !chessMatch.getCheck()) {
+			//roque do lado do rei
+			Position posT1 = new Position(position.getRow(), position.getColumn() + 3);//posição da torre do rei
+			//testar se a torre está apta para roque
+			if(testRookCastling(posT1)) {
+				//testar se as casas entre o rei e a torre estão vazias
+				Position p1 = new Position(position.getRow(), position.getColumn() + 1);
+				Position p2 = new Position(position.getRow(), position.getColumn() + 2);
+				if(getBoard().piece(p1) == null && getBoard().piece(p2) == null) {
+					//incluir na matriz esse movimento possível do rei
+					mat[position.getRow()][position.getColumn() + 2] = true;
+				}
+			}
+			//roque do lado do rainha
+			Position posT2 = new Position(position.getRow(), position.getColumn() - 4);//posição da torre da rainha
+			//testar se a torre está apta para roque
+			if(testRookCastling(posT2)) {
+				//testar se as casas entre o rei e a torre estão vazias
+				Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+				Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+				Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+				if(getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null) {
+					//incluir na matriz esse movimento possível do rei
+					mat[position.getRow()][position.getColumn() - 2] = true;
+				}
+			}
 		}
 
 		return mat;
